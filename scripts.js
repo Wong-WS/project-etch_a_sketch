9516b57aef67505c5b78a53d5c456be86d8a67f4
Wong <66583478+Wong-WS@users.noreply.github.com>
2023-10-12 12:08:24 +0800

    added resetDiv and hover effects

--- scripts.js
+++ scripts.js
@@ -1,10 +1,32 @@
 const container = document.getElementById("container");
-
+//Hovers effect (works on black color only)//
 function makeDivs(numDivs) {
     for (let d = 0; d < numDivs; d++){
         let cells = document.createElement("div");
         container.appendChild(cells)
+        cells.addEventListener("mouseover", function () {
+            this.style.backgroundColor = "black";
+        });
     }
 }
 
 makeDivs(256);
+
+
+const divs = document.querySelectorAll("div");
+//Reset Divs to white colored background//
+function resetDivs() {
+    divs.forEach((div) => {
+        div.style.backgroundColor = "white";
+    });
+}
+
+const resetBtn = document.querySelector(".resetBtn");
+//Reset Divs button//
+resetBtn.addEventListener("click", resetDivs);
+
+const newGrid = document.querySelector(".newGrid");
+
+// function newGrid() {
+    
+// }
